b0ba65bb21f732039f3a1b3f767435660f781178
Ashell Fernando <ashellz2010@gmail.com>
2018-01-29 22:03:03 -0500

    the begining

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-app.js
+var budgetController = (function() {
+    var x = 23;
+    var add = function(a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) {
+            return (add(b));
+        }
+    }
+}) ();
+
+var UIController = (function(){ 
+
+    //some code
+    
+}) ();
+
+
+var controller = (function(budgetCtrl, UICtrl){
+
+        var z = budgetCtrl .publicTest(5);
+
+        return {
+            anothePublic: function(){
+                console.log(z);
+            }
+        }
+
+}) (budgetController, UIController);

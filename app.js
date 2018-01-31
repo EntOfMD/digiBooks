03578e7122f6e064aa7b5b5a66959973dc25cbce
Ashell Fernando <ashellz2010@gmail.com>
2018-01-30 19:16:45 -0500

    created the outline

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
+//BUDGET CONTROLLER
 var budgetController = (function() {
-    var x = 23;
-    var add = function(a) {
-        return x + a;
-    }
+    
+    //some code
 
-    return {
-        publicTest: function(b) {
-            return (add(b));
-        }
-    }
 }) ();
 
+//UI CONTROLLER
 var UIController = (function(){ 
 
     //some code
     
 }) ();
 
-
+//GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
+        
+        document.querySelector('.add__btn').addEventListener('click', function(){
 
-        var z = budgetCtrl .publicTest(5);
+            //1. Get field input data
 
-        return {
-            anothePublic: function(){
-                console.log(z);
-            }
-        }
+            //2. Add the item to the budget controller
+
+            //3. Add the item to the UI
+
+            //4. Calculate the budget
+
+            //5. Display the budget
+        });
 
 }) (budgetController, UIController);

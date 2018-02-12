31a8444d75c26f88c5912fbd1b410981d75232b7
Ashell Fernando <ashellz2010@gmail.com>
2018-02-11 23:40:46 -0500

    about to start calculations | beautified

--- app.js
+++ app.js
@@ -45,11 +45,16 @@ var budgetController = (function () {
                 newItem = new Income(ID, des, val);
             };
 
-            //Push it to our data structure
+            //Push it to the data structure
             data.allItems[type].push(newItem);
 
             //Returns the new element
             return newItem;
+        },
+
+        calculateBudget: function () {
+
+
         },
         testing: function () {
             console.log(data);
@@ -185,6 +190,10 @@ var controller = (function (budgetCtrl, UICtrl) {
         }
     };
 
+
+    /////////////////////////////////////////////////////////////////////
+    //   TESTING
+    ////////////////////////////////////////////////////////////////////
     // TESTING how to save data to blobs
     // function download(data, filename, type) {
     //     var file = new Blob([data], {
@@ -207,8 +216,44 @@ var controller = (function (budgetCtrl, UICtrl) {
     //     }
     // }
 
+    // function setCookie(cname, cvalue, exdays) {
+    //     var d = new Date();
+    //     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    //     var expires = "expires="+d.toUTCString();
+    //     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    // }
+
+    // function getCookie(cname) {
+    //     var name = cname + "=";
+    //     var ca = document.cookie.split(';');
+    //     for(var i = 0; i < ca.length; i++) {
+    //         var c = ca[i];
+    //         while (c.charAt(0) == ' ') {
+    //             c = c.substring(1);
+    //         }
+    //         if (c.indexOf(name) == 0) {
+    //             return c.substring(name.length, c.length);
+    //         }
+    //     }
+    //     return "";
+    // }
+
+    // function checkCookie() {
+    //     var user = getCookie("username");
+    //     if (user != "") {
+    //         alert("Welcome again " + user);
+    //     } else {
+    //         user = prompt("Please enter your name:", "");
+    //         if (user != "" && user != null) {
+    //             setCookie("username", user, 365);
+    //         }
+    //     }
+    // }
+
 
 })(budgetController, UIController);
+
+
 
 
 
